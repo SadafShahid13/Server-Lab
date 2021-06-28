f048a1f556432da5e34326525a79453c2171bb54
Sinclair <borshon.sadafshahid@gmail.com>
2021-06-29 01:13:22 +0600

    Can't access dashboard without signing in

--- Lab_2-3_180042137/routes/userRoutes.routes.js
+++ Lab_2-3_180042137/routes/userRoutes.routes.js
@@ -20,6 +20,10 @@ userRoutes.post("/login", isLoggedIn, postLogin);
 
 userRoutes.post("/dashboard", isLoggedIn, postDashBoard);
 
+userRoutes.get("/dashboard", (req,res) =>{
+    res.send("<h1>You need to Login or Register to access the Dashboard</h1> <div><a href=/login> Login </a></div> <a href=/register> Register </a>");
+})
+
 userRoutes.get("/register", getRegister);
 
 userRoutes.post("/register", postRegister);
